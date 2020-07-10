32e7ee8a5e896fee67a74b02a7409b6a34a34c12
Mahyar <mahyar.zarnegar@brickmakers.de>
2020-07-10 10:47:52 +0200

    progress class
    functions update

--- app/src/main/java/com/example/bmweather/progress.kt
+++ app/src/main/java/com/example/bmweather/progress.kt
@@ -1,4 +1,15 @@
 package com.example.bmweather
 
+import android.view.View
+
 class progress {
+
+    fun start(progressBar: View) {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun done(progressBar: View) {
+        progressBar.visibility = View.INVISIBLE
+    }
+
 }
