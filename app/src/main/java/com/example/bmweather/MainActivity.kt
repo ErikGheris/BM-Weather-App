fee39c8b5c02ca23250d27a728edcc01f80e6c13
Mahyar <mahyar.zarnegar@brickmakers.de>
2020-08-13 11:51:16 +0200

    refactored the MainActivity
    added makeSearchWeatherRequest() , makeCurrentLocationWeatherRequest() to reduce the code lines

--- app/src/main/java/com/example/bmweather/MainActivity.kt
+++ app/src/main/java/com/example/bmweather/MainActivity.kt
@@ -10,10 +10,8 @@ import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.view.View
 import android.view.WindowManager
 import android.widget.AutoCompleteTextView
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bmweather.databinding.ActivityMainBinding
@@ -33,6 +31,8 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
     override var yCoordination: String = ""
     override var countryCode: String = ""
     override var locality: String = ""
+    private var searchedXCoordination = ""
+    private var searchedYCoordination = ""
     private val apiKey = "6133b390a077c487bc9ac43311b3ba26"
     private var cityName = ""
     private var units = "metric"
@@ -41,13 +41,11 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
     private var searched: String = ""
     private var exclude = "hourly,minutely"
     private val fetchWeather = FetchWeatherData
-    private var searchedxCoordination = ""
-    private var searchedyCoordination = ""
     private lateinit var lastLocation: LastLocation
     private lateinit var binding: ActivityMainBinding
     private var searching = false
 
-    private var load:Load = Load()
+    private var load: Load = Load()
     // TODO: 12.08.20   lazy declarataion vs inFunctionDeclaration
     //  val list : ArrayList by lazy { ArrayList() }
     // private lateinit var backToast: Toast
@@ -84,6 +82,7 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
         searchButtonAction()
         swipeAction()
         activityButtonAction()
+
     }
 
 
@@ -93,15 +92,7 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
         if (xCoordination.isEmpty()) {
             Handler().postDelayed({
                 binding.city.text = getString(R.string.City, locality, countryCode)
-                fetchWeather.getCurrentWeatherReport(
-                    app_id = apiKey,
-                    lat = xCoordination,
-                    lon = yCoordination,
-                    lang = lang,
-                    units = units,
-                    exclude = exclude,
-                    mainActivity = this
-                )
+                makeCurrentLocationWeatherRequest()
                 Toast.makeText(
                     this, "Data Updated, Coordinates are $xCoordination, $yCoordination",
                     Toast.LENGTH_SHORT
@@ -110,7 +101,7 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
                 swipe.isRefreshing = false
 
                 window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-              load.done(binding.Progress)
+                load.done(binding.Progress)
             }, 4000)
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
@@ -119,15 +110,7 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
         } else {
             if (!searching) {
                 binding.city.text = getString(R.string.City, locality, countryCode)
-                fetchWeather.getCurrentWeatherReport(
-                    app_id = apiKey,
-                    lat = xCoordination,
-                    lon = yCoordination,
-                    lang = lang,
-                    units = units,
-                    exclude = exclude,
-                    mainActivity = this
-                )
+                makeCurrentLocationWeatherRequest()
                 Toast.makeText(
                     this, "Data Updated, Coordinates are $xCoordination, $yCoordination",
                     Toast.LENGTH_SHORT
@@ -136,23 +119,15 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
                 swipe.isRefreshing = false
 
                 window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-             load.done(binding.Progress)
+                load.done(binding.Progress)
 
             } else {
                 lastCityCache = cityName
                 cityName = searched
-                searchedxCoordination = lastLocation.toLatitude(cityName)
-                searchedyCoordination = lastLocation.toLongitude(cityName)
+                searchedXCoordination = lastLocation.toLatitude(cityName)
+                searchedYCoordination = lastLocation.toLongitude(cityName)
                 setSearchedCityInfoInTV()
-                fetchWeather.getCurrentWeatherReport(
-                    apiKey,
-                    lat = searchedxCoordination,
-                    lon = searchedyCoordination,
-                    lang = lang,
-                    units = units,
-                    exclude = exclude,
-                    mainActivity = this
-                )
+                makeSearchWeatherRequest()
                 load.done(binding.Progress)
             }
         }
@@ -178,22 +153,14 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
             if (searched.trim().isNotEmpty()) {
                 lastCityCache = cityName
                 cityName = searched
-                searchedxCoordination = lastLocation.toLatitude(cityName)
-                searchedyCoordination = lastLocation.toLongitude(cityName)
+                searchedXCoordination = lastLocation.toLatitude(cityName)
+                searchedYCoordination = lastLocation.toLongitude(cityName)
                 setSearchedCityInfoInTV()
-                fetchWeather.getCurrentWeatherReport(
-                    apiKey,
-                    lat = searchedxCoordination,
-                    lon = searchedyCoordination,
-                    lang = lang,
-                    units = units,
-                    exclude = exclude,
-                    mainActivity = this
-                )
+                makeSearchWeatherRequest()
                 //safe city
                 Toast.makeText(
                     this,
-                    "looking for $searched's Weather Info, Coordinates are $searchedxCoordination $searchedyCoordination",
+                    "looking for $searched's Weather Info, Coordinates are $searchedXCoordination $searchedYCoordination",
                     Toast.LENGTH_SHORT
                 )
                     .show()
@@ -208,6 +175,29 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
         }
     }
 
+    private fun makeSearchWeatherRequest() {
+        fetchWeather.getCurrentWeatherReport(
+            apiKey,
+            lat = searchedXCoordination,
+            lon = searchedYCoordination,
+            lang = lang,
+            units = units,
+            exclude = exclude,
+            mainActivity = this
+        )
+    }
+    private fun makeCurrentLocationWeatherRequest() {
+        fetchWeather.getCurrentWeatherReport(
+            app_id = apiKey,
+            lat = xCoordination,
+            lon = yCoordination,
+            lang = lang,
+            units = units,
+            exclude = exclude,
+            mainActivity = this
+        )
+    }
+
 
     private fun activityButtonAction() {
         binding.activityButton.setOnClickListener {
@@ -215,12 +205,12 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
             if (!searching) {
                 intent.putExtra("xCoordination", xCoordination)
                 intent.putExtra("yCoordination", yCoordination); } else {
-                intent.putExtra("xCoordination", searchedxCoordination)
-                intent.putExtra("yCoordination", searchedyCoordination)
+                intent.putExtra("xCoordination", searchedXCoordination)
+                intent.putExtra("yCoordination", searchedYCoordination)
             }
             Toast.makeText(
                 this,
-                "Forcast for:  $searchedxCoordination, $searchedyCoordination",
+                "Forcast for:  $searchedXCoordination, $searchedYCoordination",
                 Toast.LENGTH_SHORT
             ).show()
             startActivity(intent)
@@ -233,15 +223,7 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
         binding.swipe.setOnRefreshListener {
             searching = false
             binding.city.text = getString(R.string.City, locality, countryCode)
-            fetchWeather.getCurrentWeatherReport(
-                app_id = apiKey,
-                lat = xCoordination,
-                lon = yCoordination,
-                lang = lang,
-                units = units,
-                exclude = exclude,
-                mainActivity = this
-            )
+            makeCurrentLocationWeatherRequest()
             Toast.makeText(
                 this, "Data Updated, Coordinates are $xCoordination, $yCoordination",
                 Toast.LENGTH_SHORT
@@ -250,6 +232,8 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
             swipe.isRefreshing = false
         }
     }
+
+
 
     private fun setSearchedCityInfoInTV() {
         val (locale, countryCode) = getCityInfo()
@@ -340,34 +324,6 @@ class MainActivity : AppCompatActivity(), LocationReceiver {
                 }
             }
         }
-    }
-
-
-    private fun networkCheck(context: Context): Boolean {
-        val statusCheck = networkAvailabilityStatus(context)
-        return if (!statusCheck) {
-            Toast.makeText(context, "Internet is not Available", Toast.LENGTH_SHORT).show()
-            false
-        } else
-            true
-    }
-
-
-    private fun networkAvailabilityStatus(context: Context): Boolean {
-        val result: Boolean
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkCapabilities = connectivityManager.activeNetwork ?: return false
-        val activeNetwork =
-            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
-        result = when {
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
-        }
-
-        return result
     }
 
 
--- app/src/main/java/com/example/bmweather/Network/ConnectivityManagement.kt
+++ app/src/main/java/com/example/bmweather/Network/ConnectivityManagement.kt
@@ -1,4 +1,44 @@
 package com.example.bmweather.Network
 
-class ConnectivityManagement {
+import android.content.Context
+import android.content.res.Resources
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.provider.Settings.Global.getString
+import android.provider.Settings.Secure.getString
+import android.widget.Toast
+import androidx.core.content.res.TypedArrayUtils.getString
+import com.example.bmweather.R
+
+class ConnectivityManagement(context: Context) {
+private val connectionStatusToast:Toast =  Toast.makeText(context, Resources.getSystem().getString(R.string.no_internet), Toast.LENGTH_SHORT)
+
+
+ fun networkCheck(context: Context): Boolean {
+        val statusCheck = networkAvailabilityStatus(context)
+        return if (!statusCheck) {
+            connectionStatusToast.show()
+            false
+        } else
+            true
+    }
+
+
+ private fun networkAvailabilityStatus(context: Context): Boolean {
+        val result: Boolean
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val activeNetwork =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+
+        return result
+    }
+
 }
