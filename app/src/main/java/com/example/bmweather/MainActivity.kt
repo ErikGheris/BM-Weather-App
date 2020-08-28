ca2c4975305159de1296f3f7431225bc44dafce8
Erik <erik.gheris@brickmakers.de>
2020-08-28 14:58:41 +0200

    [Merge Conflicts][Solved]

--- app/src/main/java/com/example/bmweather/MainActivity.kt
+++ app/src/main/java/com/example/bmweather/MainActivity.kt
@@ -193,6 +193,7 @@ class MainActivity : AppCompatActivity(),
                         Toast.LENGTH_SHORT
                     ).show()
                 }
+                
             } else{
                 Toast.makeText(
                     this,
